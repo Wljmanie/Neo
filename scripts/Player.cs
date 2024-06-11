0466218e85b0e78b1158d181d4ae492528603df5
Wesley Manie <wljmanie@gmail.com>
2024-06-11 13:50:25 +0200

    Player Moves

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	private const int cellsize = 64;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 		GD.Print("Do I print?");
 	}
 
@@ -20,18 +22,22 @@ public partial class Player : CharacterBody2D
 		{
 			if(keyEvent.Keycode == Key.H)
 			{
+				this.Position = new Vector2(this.Position.X - cellsize, this.Position.Y);
 				GD.Print("H");
 			}
 			if(keyEvent.Keycode == Key.J)
 			{
+				this.Position = new Vector2(this.Position.X, this.Position.Y + cellsize);
 				GD.Print("J");
 			}       
 			if(keyEvent.Keycode == Key.K)
 			{
+				this.Position = new Vector2(this.Position.X, this.Position.Y - cellsize);
 				GD.Print("K");
 			}
 			if(keyEvent.Keycode == Key.L)
 			{
+				this.Position = new Vector2(this.Position.X + cellsize, this.Position.Y);
 				GD.Print("L");
 			}
 		}
